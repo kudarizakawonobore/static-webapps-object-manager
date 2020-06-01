539728e5a80c0f676c122ce5dae2c3d7cf467388
kudarizakawonobore <kudarizakawonobore@gmail.com>
2020-06-01 09:03:23 +0900

    コメント削除

--- api/ReadDevices/index.js
+++ api/ReadDevices/index.js
@@ -4,15 +4,6 @@ let tableSvc = storage.createTableService(process.env.STORAGE_ACCOUNT, process.e
 module.exports = async function (context, req) {
 
     if (req.method == "GET") {
-        // context.log("---------------")
-        // let query = new storage.TableQuery()
-        //     .where('PartitionKey eq ?', 'devices');
-        // tableSvc.queryEntities(process.env.TABLE, query, null, (error, result, response) => {
-        //     context.log(response.body.value)
-        //     context.res.json(response.body.value);
-        //     context.res.body = "hoge"
-        //     context.done();
-        // });
         context.res = {
             status: 200,
             body: context.bindings.deviceEntity
